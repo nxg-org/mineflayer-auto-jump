aedd17e161e2c96d9a0ce57636accd9520b3b83a
GenerelSchwerz <xXSG.KingXx@gmail.com>
2023-01-14 17:03:31 -0500

    updating

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -4,29 +4,62 @@ import { JumpChecker } from "./autoJumper";
 
 
 
+export interface AutoJumperOpts {
+    autoJump: boolean,
+}
+
+const defaultKeys: AutoJumperOpts = {
+    autoJump: false
+}
 
 
-export class AutoJumper extends EventEmitter {
+
+export class AutoJumper extends EventEmitter implements AutoJumperOpts {
 
     private handler: JumpChecker;
+    
+    private _autoJump: boolean;
 
-
-    private _automaticallyJump: boolean;
-
-
-    public get automaticallyJump() {
-        return this._automaticallyJump;
+    public get autoJump() {
+        return this._autoJump;
     }
 
+    public set autoJump(jump: boolean) {
 
-    public set automaticallyJump(jump: boolean) {
-        
+        if (jump) this.initListeners();
+        else this.cleanupListeners();
+        this._autoJump = jump;
     }
 
-
-    public constructor(private bot: Bot) {
+    public constructor(private bot: Bot, opts: Partial<AutoJumperOpts> = {}) {
         super();
         this.handler = new JumpChecker(bot);
+        this.setOpts(Object.assign({}, defaultKeys, opts))
     }
 
+    /**
+     * 
+     * @param opts 
+     */
+    public setOpts(opts: Partial<AutoJumperOpts>) {
+        for (const key in opts) {
+            if (key in this && key in defaultKeys) {
+                // @ts-expect-error
+                this[key] = opts[key];
+            }
+        }
+    }
+
+
+
+    private initListeners() {
+
+    }
+
+    private cleanupListeners() {
+
+    }
 }
+
+
+
